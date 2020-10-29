3365cbae71add9748c096f72b8f71cc90fe4031b
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-10-29 23:40:47 +0000

    Update background.js

--- background.js
+++ background.js
@@ -2,6 +2,7 @@ chrome.webRequest.onHeadersReceived.addListener(function(details) {
   for (let i = 0; i < details.responseHeaders.length; i += 1) {
     if (details.responseHeaders[i].name.toLowerCase() === "x-frame-options") {
 		details.responseHeaders.push({name: "Cross-Origin-Opener-Policy", value: "same-origin"});
+	        break
     }
   }
 return {responseHeaders: details.responseHeaders};
