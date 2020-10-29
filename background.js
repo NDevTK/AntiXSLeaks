6a51998abca11073187ac78ad6d41f9929855dc6
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-10-29 00:26:33 +0000

    Create background.js

---
+++ background.js
@@ -0,0 +1,8 @@
+chrome.webRequest.onHeadersReceived.addListener(function(details) {
+  for (let i = 0; i < details.responseHeaders.length; i += 1) {
+    if (details.responseHeaders[i].name.toLowerCase() === "x-frame-options") {
+		details.responseHeaders.push({name: "Cross-Origin-Opener-Policy", value: "same-origin"});
+    }
+  }
+return {responseHeaders: details.responseHeaders};
+}, {urls: ["<all_urls>"]}, ["blocking", "responseHeaders"]);
