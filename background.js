a7217d4d64e0cf5673e6ff6a88a4ede6f5ea9ef0
NDevTK <31563761+NDevTK@users.noreply.github.com>
2021-12-02 19:23:28 +0000

    Update background.js

--- background.js
+++ background.js
@@ -12,8 +12,6 @@ const headers = [
 
 // Require direct URL access by user or the same-origin.
 const protectedOrigins = new Set(["https://example.com", "https://myaccount.google.com", "https://payments.google.com", "https://myactivity.google.com", "https://pay.google.com", "https://adssettings.google.com", "https://mail.google.com", "https://mail.protonmail.com", "https://account.protonmail.com", "https://outlook.live.com"]);
-// Orgins that are exempt from protocol limit.
-const protectedProtocolsBypass = new Set([]);
 
 const exceptions = new Map()
 .set("https://account-api.protonmail.com", ['x-frame-options'])
