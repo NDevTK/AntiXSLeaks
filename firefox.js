efb3869e10ab07210edda22b435b61176de619b0
NDevTK <31563761+NDevTK@users.noreply.github.com>
2023-09-04 04:41:02 +0100

    Update firefox.js

--- firefox.js
+++ firefox.js
@@ -1,9 +1,1 @@
-async function confirm(message) {
-  let popup = await browser.windows.create({type: 'popup', url: "confirm.html"});
-  await new Promise(resolve => setTimeout(resolve, 100));
-  let result = await browser.tabs.sendMessage(popup.tabs[0].id, message);
-  browser.windows.remove(popup.id);
-  return result;
-}
-
 firefox = true;
